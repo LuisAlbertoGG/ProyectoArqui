1b55f45d0d90a3d227ade111ef3fc011ff77386e
AlejandroVaGa <ale.valdes@ciencias.unam.mx>
2016-06-30 19:00:55 -0500

    Update operaciones.py

--- operaciones.py
+++ operaciones.py
@@ -1,47 +1,52 @@
-def operacion3 (op, a1, a2, a3):
-	if(op == 'add'):
-		a1 = a2 + a2
+def op(array):
+	if(array[0] == 'add'):
+		array[1] = array[2] + array[3]
+		ciclos += 3
 	elif(op == 'sub'):
-		a1 = a2 - a2
+		array[1] = array[2] - array[3]
+		ciclos += 4
+	elif(op == 'mul'):
+		array[1] = array[2] * array[3]
+		ciclos += 10
+	elif(op == 'div'):
+		array[1] = array[2] / array[3]
+		ciclos += 11
+		
 	elif(op == 'fadd'):
-		float(a1)
-		float(a2)
-		float(a3)
-		a1 = a2 + a3
+		float(array[1])
+		float(array[2])
+		float(array[3])
+		array[1] = array[2] + array[3]
+		ciclos += 4
 	elif(op == 'fsub'):
-		float(a1)
-		float(a2)
-		float(a3)
-		a1 = a2 - a3
-	elif(op == 'and'):
-		a1 = a2 and a3
-	elif(op == 'or'):
-		a1 = a2 or a3
+		float(array[1])
+		float(array[2])
+		float(array[3])
+		array[1] = array[2] - array[3]
+		ciclos += 5
+	elif(op == 'fmul'):
+		float(array[1])
+		float(array[2])
+		float(array[3])
+		array[1] = array[2] * array[3]
+		ciclos += 9
+	elif(op == 'fdiv'):
+		float(array[1])
+		float(array[2])
+		float(array[3])
+		array[1] = array[2] / array[3]
+		ciclos += 10
+	
+    elif(op == 'and'):
+        array[1] = array[2] and array[3]
+		ciclos += 1
+    elif(op == 'or'):
+        array[1] = array[2] or array[3]
+		ciclos += 1
 	elif(op == 'xor'):
-		a1 = (a2 and a3) or (not(a2) and not(a3))
-	elif(op == 'lb'):
-		a1 = memoria[a2 + a3]
-	elif(op == 'lw'):
-		a1 = memoria[a2 + a3]
-	elif(op == 'sb'):
-		memoria[a2 + a3] = a1
-	elif(op == 'sw'):
-		memoria[a2 + a3] = a1
-
-def operacion2(op, a1, a2):
-  if(op == 'mult'):
-    LO = a1 * a2
-    HI = a1 * a2
-  elif(op == 'div'):
-    LO  = a1 * a2
-    HI = a1 % a2
-  elif(op == 'fmult'):
-    float(a1)
-    float(a2)
-    LO = a1 * a2
-    HI = a1 * a2
-  elif(op == 'fdiv'):
-    float(a1)
-    float(a2)
-    LO = a1 * a2
-    HI = a1 % a2
+		array[1] = (array[2] and array[3]) or (not(array[2]) and not(array)[3]))
+		ciclos += 1
+	elif(op == 'not')
+		#a = m[pc + 3];
+		array[1] = not(a2)
+		ciclos += 1
